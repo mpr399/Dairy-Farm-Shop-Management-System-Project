c37c6a35cb1ca858714ea89598e9d17eff365a6e
mpr399 <mprdevops8@gmail.com>
2024-10-12 20:57:17 +0530

    first commit

--- app/src/dfsms/includes/config.php
+++ app/src/dfsms/includes/config.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "dfsms");
+$con=mysqli_connect("dfsmsdb-server", "root", "Mpr@399", "dfsms");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
